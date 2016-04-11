5f424756481feda1180af4e29527e1885c52261a
Nick Rosati <rosati@email.arizona.edu>
2016-04-11 14:32:51 -0700

    Skeleton Files

---
+++ src/DES_Skeleton.java
@@ -0,0 +1,151 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+import gnu.getopt.Getopt;
+
+
+public class DES_Skeleton {
+
+	public static void main(String[] args) {
+		
+		StringBuilder inputFile = new StringBuilder();
+		StringBuilder outputFile = new StringBuilder();
+		StringBuilder keyChainFile = new StringBuilder();
+		StringBuilder encrypt = new StringBuilder();
+		
+		pcl(args, inputFile, outputFile, keyChainFile, encrypt);
+		
+		if(keyChainFile.toString() != "" && encrypt.toString().equals("e")){
+			encrypt(keyChainFile, inputFile, outputFile);
+		} else if(keyChainFile.toString() != "" && encrypt.toString().equals("d")){
+			decrypt(keyChainFile, inputFile, outputFile);
+		}
+		
+		
+	}
+	
+
+	private static void decrypt(StringBuilder keyChainFile, StringBuilder inputFile,
+			StringBuilder outputFile) {
+		try {
+			PrintWriter writer = new PrintWriter(outputFile.toString(), "UTF-8");
+			List<String> lines = Files.readAllLines(Paths.get(inputFile.toString()), Charset.defaultCharset());
+			String IVStr = lines.get(0);
+			lines.remove(0);
+			String encryptedText;
+			
+			for (String line : lines) {
+				encryptedText = DES_decrypt(IVStr, line);
+				writer.print(encryptedText);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	/**
+	 * TODO: You need to write the DES encryption here.
+	 * @param line
+	 */
+	private static String DES_decrypt(String iVStr, String line) {
+		
+		return null;
+	}
+
+
+	private static void encrypt(StringBuilder keyStr, StringBuilder inputFile,
+			StringBuilder outputFile) {
+		
+		try {
+			PrintWriter writer = new PrintWriter(outputFile.toString(), "UTF-8");
+			
+			String encryptedText;
+			for (String line : Files.readAllLines(Paths.get(inputFile.toString()), Charset.defaultCharset())) {
+				encryptedText = DES_encrypt(line);
+				writer.print(encryptedText);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		
+	}
+	/**
+	 * TODO: You need to write the DES encryption here.
+	 * @param line
+	 */
+	private static String DES_encrypt(String line) {
+		
+		return null;
+	}
+
+
+	static void genDESkey(){
+		System.out.println("New key goes here");
+		return;
+	}
+
+
+	/**
+	 * This function Processes the Command Line Arguments.
+	 * -p for the port number you are using
+	 * -h for the host name of system
+	 */
+	private static void pcl(String[] args, StringBuilder inputFile,
+							StringBuilder outputFile, StringBuilder keyChainFile, StringBuilder encrypt) {
+		/*
+		 * http://www.urbanophile.com/arenn/hacking/getopt/gnu.getopt.Getopt.html
+		*/	
+		Getopt g = new Getopt("Chat Program", args, "hke:d:i:o:");
+		int c;
+		String arg;
+		while ((c = g.getopt()) != -1){
+		     switch(c){
+		     	  case 'o':
+		        	  arg = g.getOptarg();
+		        	  outputFile.append(arg);
+		        	  break;
+		     	  case 'i':
+		        	  arg = g.getOptarg();
+		        	  inputFile.append(arg);
+		        	  break;
+	     	  	  case 'e':
+		        	  arg = g.getOptarg();
+		        	  keyChainFile.append(arg);
+		        	  encrypt.append("e");
+		        	  break;
+	     	  	  case 'd':
+		        	  arg = g.getOptarg();
+		        	  keyChainFile.append(arg);
+		        	  encrypt.append("d");
+		        	  break;
+		          case 'k':
+		        	  genDESkey();
+		        	  break;
+		          case 'h':
+		        	  callUseage(0);
+		          case '?':
+		            break; // getopt() already printed an error
+		            //
+		          default:
+		              break;
+		       }
+		   }
+		
+	}
+	
+	private static void callUseage(int exitStatus) {
+		
+		String useage = "";
+		
+		System.err.println(useage);
+		System.exit(exitStatus);
+		
+	}
+	
+}
---
+++ src/RSA_skeleton.java
@@ -0,0 +1,106 @@
+import gnu.getopt.Getopt;
+
+
+public class RSA_skeleton {
+
+	public static void main(String[] args){
+		
+		StringBuilder bitSizeStr = new StringBuilder();
+		StringBuilder keyChainFile = new StringBuilder();
+		StringBuilder m = new StringBuilder();
+		StringBuilder encrypt = new StringBuilder();
+		
+		pcl(args, bitSizeStr, keyChainFile,m, encrypt);
+		
+//		You are going to have to pull out the numbers from key Chain File
+//		You can do this any way you want, this is just a suggested setup.
+		StringBuilder eStr = new StringBuilder();
+		StringBuilder nStr = new StringBuilder();
+		StringBuilder dStr = new StringBuilder();
+		
+		if(!bitSizeStr.toString().equalsIgnoreCase("")){
+			//This means you want to create a new key
+			genRSAkey(bitSizeStr);
+		}
+		
+		if(!eStr.toString().equalsIgnoreCase("")){
+			RSAencrypt(m, nStr, eStr);
+		}
+		
+		if(!dStr.toString().equalsIgnoreCase("")){
+			RSAdecrypt(m, nStr, dStr);
+		}
+		
+		
+	}
+
+
+
+	private static void RSAencrypt(StringBuilder m, StringBuilder nStr, StringBuilder eStr) {
+		// TODO Auto-generated method stub
+	}
+
+	private static void RSAdecrypt(StringBuilder cStr, StringBuilder nStr,
+			StringBuilder dStr){
+		// TODO Auto-generated method stub
+	}
+	
+	private static void genRSAkey(StringBuilder bitSizeStr) {
+		// TODO Auto-generated method stub
+	}
+
+
+	/**
+	 * This function Processes the Command Line Arguments.
+	 */
+	private static void pcl(String[] args, StringBuilder bitSizeStr,
+							StringBuilder keyChainFile , StringBuilder m, StringBuilder encrypt) {
+		/*
+		 * http://www.urbanophile.com/arenn/hacking/getopt/gnu.getopt.Getopt.html
+		*/	
+		Getopt g = new Getopt("Chat Program", args, "hke:d:i:");
+		int c;
+		String arg;
+		while ((c = g.getopt()) != -1){
+		     switch(c){
+		     	  case 'i':
+		        	  arg = g.getOptarg();
+		        	  m.append(arg);
+		        	  break;
+		          case 'e':
+		        	  arg = g.getOptarg();
+		        	  keyChainFile.append(arg);
+		        	  encrypt.append("e");
+		        	  break;
+		     	  case 'd':
+		        	  arg = g.getOptarg();
+		        	  keyChainFile.append(arg);
+		        	  encrypt.append("d");
+		        	  break;
+		          case 'k':
+		        	  break;
+		     	  case 'b':
+		        	  arg = g.getOptarg();
+		        	  bitSizeStr.append(arg);
+		        	  break;
+		          case 'h':
+		        	  callUsage(0);
+		          case '?':
+		            break; // getopt() already printed an error
+		          default:
+		              break;
+		       }
+		   }
+	}
+	
+	private static void callUsage(int exitStatus) {
+
+		String useage = "";
+		
+		System.err.println(useage);
+		System.exit(exitStatus);
+		
+	}
+
+
+}
