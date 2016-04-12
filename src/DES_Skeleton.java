d612b4dba1300ab1005c55c243ba1395be1b16ba
Nick Rosati <rosati@email.arizona.edu>
2016-04-11 19:16:43 -0700

    Updated DES key think it matches the spec now, again just can't test until Opt stuff gets figured out

--- src/DES_Skeleton.java
+++ src/DES_Skeleton.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.BitSet;
 import java.math.BigInteger;
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.Base64;
 import java.util.Base64.Encoder;
 import java.util.
@@ -91,14 +92,14 @@ public class DES_Skeleton {
 	}
 
 	/*
-	 * Generate DES key.  DES functional key is 56 bits.  Its a 64 bit key where the 8th bit in each byte is omitted.
-	 * I dont know how to do that so right now we're just getting 56 bits.  The spec does say its supposed to take a 
-	 * 64 bit key from a file on the command line so we will have to revist this.  Then we encode it in Base64.  
-	 * Only reason why we do that is because in the spec it says to use Base64 its useful encoding.  
-	 * Then we print in Hex because again thats what the spec says to do.  Not sure if the format is correct
-	 * for the hex printing cant test until the getopt stuff gets figured out.  Spec doesnt say anytning but Im pretty
-	 * sure the key is supposed to get written to the key file.  Also we may want to check to make sure we didnt generate
-	 * a weak key.  I think there is a list on wikipedia of keys that are considered weak.
+	 * Generate DES key.  DES functional key is 56 bits.  Its a 64 bit key where the 8th bit in each byte is for parity.
+	 * I dont know how to do that, or if we have to do that, so right now we're just getting 56 bits.  The spec does say 
+	 * its supposed to take a 64 bit key from a file on the command line but it doesnt say we write this key
+	 * to a file so who knows.  They may have test files already made up and they just use that key and this is just
+	 * to see if we can generate a key even though the key wont be used. Then we encode it in Base64. Only reason why we do 
+	 * that is because in the spec it says to use Base64 its useful encoding.Then we print in Hex because again thats what the 
+	 * spec says to do.  Not sure if the format is correct for the hex printing cant test until the getopt stuff gets figured 
+	 * out.  Comparing to weak key is very sloppy but I couldnt think of a smarter way to do it.
 	 */
 	static void genDESkey(){
 		//System.out.println("New key goes here");
@@ -106,11 +107,27 @@ public class DES_Skeleton {
 		
 		byte[] bytes = new byte[7];//Change to 8 to get 64 bit key
 		random.nextBytes(bytes);//Get 7 Random bytes = 56 bits
+		byte[] weak1 = {(byte)0x01, (byte)0x01, (byte)0x01, (byte)0x01, (byte)0x01, (byte)0x01, (byte)0x01, (byte)0x01};
+		byte[] weak2 = {(byte)0xFE, (byte)0xFE, (byte)0xFE, (byte)0xFE, (byte)0xFE, (byte)0xFE, (byte)0xFE, (byte)0xFE};
+		byte[] weak3 = {(byte)0xE0, (byte)0xE0, (byte)0xE0, (byte)0xE0, (byte)0xF1, (byte)0xF1, (byte)0xF1, (byte)0xF1};
+		byte[] weak4 = {(byte)0x1F, (byte)0x1F, (byte)0x1F, (byte)0x0E, (byte)0x0E, (byte)0x0E, (byte)0x0E, (byte)0x0E};
+		byte[] weak5 = {(byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00, (byte)0x00};
+		byte[] weak6 = {(byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF, (byte)0xFF};
+		byte[] weak7 = {(byte)0xE1, (byte)0xE1, (byte)0xE1, (byte)0xE1, (byte)0xF0, (byte)0xF0, (byte)0xF0, (byte)0xF0};
+		byte[] weak8 = {(byte)0x1E, (byte)0x1E, (byte)0x1E, (byte)0x0F, (byte)0x1E, (byte)0x1E, (byte)0x1E, (byte)0x1E};
 		
-		String key = Base64.getEncoder().encodeToString(bytes);//Encode the Key
-		System.out.println(String.format("0x08x", key));//Print as hex
+		if(Arrays.equals(bytes, weak1) | Arrays.equals(bytes,  weak2) | Arrays.equals(bytes,  weak3) | 
+				Arrays.equals(bytes,  weak4) | Arrays.equals(bytes,  weak5) | Arrays.equals(bytes,  weak6) | 
+				Arrays.equals(bytes,  weak7) | Arrays.equals(bytes,  weak8) )
+		{
+				genDESkey();
+		}
+		else
+		{
+			String key = Base64.getEncoder().encodeToString(bytes);//Encode the Key
+			System.out.println(String.format("0x08x", key));//Print as hex
+		}
 		
-		//keyChainFile.append(key); write to file?
 		return;
 	}
 
