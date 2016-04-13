b0a805ef2003af922ab18883625b358bbac5cf39
Nick Rosati <rosati@email.arizona.edu>
2016-04-12 18:37:02 -0700

    Generate Key Done

--- src/DES_Skeleton.java
+++ src/DES_Skeleton.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -88,23 +89,22 @@ public class DES_Skeleton {
 	 */
 	private static String DES_encrypt(String line,StringBuilder keyChain) {
 		String key = keyChain.toString();
-		byte[] bytes = key.getBytes("UTF-8");
+		try {
+			byte[] bytes = key.getBytes("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return null;
 	}
 
 	/*
-	 * Generate DES key.  DES functional key is 56 bits.  Its a 64 bit key where the 8th bit in each byte is for parity.
-	 * I dont know how to do that, or if we have to do that, so right now we're just getting 56 bits.  The spec does say 
-	 * its supposed to take a 64 bit key from a file on the command line but it doesnt say we write this key
-	 * to a file so who knows.  They may have test files already made up and they just use that key and this is just
-	 * to see if we can generate a key even though the key wont be used. Then we encode it in Base64. Only reason why we do 
-	 * that is because in the spec it says to use Base64 its useful encoding.Then we print in Hex because again thats what the 
-	 * spec says to do.  Not sure if the format is correct for the hex printing cant test until the getopt stuff gets figured 
-	 * out.  Comparing to weak key is very sloppy but I couldnt think of a smarter way to do it.
+	 * Generates a 56 bit key, checks it against list of weak keys from wikipedia, if it is a match calls itself
+	 * if not prints the key in hexadecimal.
 	 */
 	static void genDESkey(){
-		//System.out.println("New key goes here");
+		
 		SecureRandom random = new SecureRandom();  //Create RNG instance
 		
 		byte[] bytes = new byte[7];//Change to 8 to get 64 bit key
@@ -127,7 +127,7 @@ public class DES_Skeleton {
 		else
 		{
 			String key = Base64.getEncoder().encodeToString(bytes);//Encode the Key
-			System.out.println(String.format("0x08x", key));//Print as hex
+			System.out.println(key.format("%02x", new BigInteger(1, key.getBytes())));//Print as hex
 		}
 		
 		return;
